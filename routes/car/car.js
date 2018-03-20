47b82e6c8900b768d9a7b0bb643f1a4fc645fe1e
AndersDLund <32234005+AndersDLund@users.noreply.github.com>
2018-03-20 17:31:10 -0500

    heroku

--- routes/car/car.js
+++ routes/car/car.js
@@ -15,7 +15,7 @@ router.get('/:id', function(req, res) {
   console.log(id);
   knex('user_car')
   .where('user_id', id)
-    .innerJoin('car_model', 'user_car.model_id', 'car_model.id')
+    .innerJoin('car_model', 'user_car.model_id', 'poop', as, 'car_model.id')
 
   // .innerJoin('car_model', 'user_car.model_id', 'car_model.id')
   // .innerJoin('car_make', 'car_model.make_id', 'car_make.id')
