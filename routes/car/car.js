c5ff8f0755b0541942aac18830f506d7104f3e16
AndersDLund <32234005+AndersDLund@users.noreply.github.com>
2018-03-20 16:09:09 -0500

    heroku stuff

--- routes/car/car.js
+++ routes/car/car.js
@@ -14,7 +14,7 @@ router.get('/:id', function(req, res) {
   const id = filterInt(req.params.id)
   console.log(id);
   knex('user_car')
-  .select('*')
+  .select('user_car.id')
   .where('user_id', id)
   .fullOuterJoin('car_model', 'user_car.model_id', 'car_model.id')
   .fullOuterJoin('car_make', 'car_model.make_id', 'car_make.id')
