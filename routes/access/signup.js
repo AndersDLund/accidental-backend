338acdeb9bd999566a179534b1c785262ef56af9
AndersDLund <32234005+AndersDLund@users.noreply.github.com>
2018-03-19 22:19:54 -0500

    changed env

--- routes/access/signup.js
+++ routes/access/signup.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const config = require('../../knexfile.js')['development'];
+const config = require('../../knexfile.js')['production'];
 const knex = require('knex')(config);
 const bcrypt = require('bcrypt');
 const salt = bcrypt.genSaltSync(8);
