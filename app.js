90ce4d9c1ef1b9697d3f3732a1caab93e5954f3f
AndersDLund <32234005+AndersDLund@users.noreply.github.com>
2018-03-19 22:09:44 -0500

    adde a start

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ const app = express();
 
 //Setting up session
 app.use(session({
-    secret: 'drinking all the wine',
+    secret: 'drinking',
     resave: false,
     saveUninitialized: true,
     cookie: {
@@ -80,7 +80,7 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'production' ? err : {};
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.sendStatus(err.status || 500);
