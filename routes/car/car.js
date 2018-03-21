2066f8e6a23bbceabdfdfb9a5715161408e7f5c1
AndersDLund <32234005+AndersDLund@users.noreply.github.com>
2018-03-20 20:44:26 -0500

    heroku

--- routes/car/car.js
+++ routes/car/car.js
@@ -18,7 +18,7 @@ router.get('/:id', function(req, res) {
 
 
   .leftJoin('car_model', 'user_car.model_id', 'car_model.id')
-  .innerJoin('car_make', 'car_model.make_id', 'car_make.id')
+  .leftJoin('car_make', 'car_model.make_id', 'car_make.id')
 
 
     .then((car) => {
