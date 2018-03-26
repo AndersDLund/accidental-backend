72ffc4cfcb9d0309f7d75aaa24a060625737db90
AndersDLund <32234005+AndersDLund@users.noreply.github.com>
2018-03-25 19:57:50 -0500

    adding route

--- routes/damage/delete.js
+++ routes/damage/delete.js
@@ -3,14 +3,14 @@ const router = express.Router();
 const config = require('../../knexfile.js')['production'];
 const knex = require('knex')(config);
 
-router.delete('/:id', function(req, res) {
+router.put('/:id', function(req, res) {
   const id = req.params.id;
   //knex logic
   knex('car_damages')
     .where('user_car_id', id)
     .then((damage)=>{
+      knex(damage.damage_type_id, req.body.damage_type_id)
       console.log(damage[0].damage_type_id, "DAMAGE!!!!")
-      .del()
     })
 
     .then(() => {
