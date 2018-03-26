7bf2baa7183e058f883ce6ef3385b0f7e755ba0b
AndersDLund <32234005+AndersDLund@users.noreply.github.com>
2018-03-26 10:25:47 -0500

    update

--- routes/damage/delete.js
+++ routes/damage/delete.js
@@ -14,6 +14,7 @@ router.delete('/:id', function(req, res) {
       .where('damage_type_id', req.body.damage_type_id)
       .first()
       .then((delThis)=>{
+        console.log(delThis);
       knex('car_damages')
       .del(delThis)
       })
